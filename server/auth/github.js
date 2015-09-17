8482181f502d5ed98d5f5f99899d069179813b88
KyleRattet <krattet@gmail.com>
2015-09-17 16:26:51 -0600

    upated callback url

--- server/auth/github.js
+++ server/auth/github.js
@@ -17,7 +17,7 @@ var GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET
 passport.use(new GitHubStrategy({
     clientID: GITHUB_CLIENT_ID,
     clientSecret: GITHUB_CLIENT_SECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+    callbackURL: "https://kyle-auth.herokuapp.com/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     console.log(profile);
